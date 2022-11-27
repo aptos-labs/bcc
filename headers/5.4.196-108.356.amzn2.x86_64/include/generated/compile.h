e61dbf1996f579aabce1712229ddb5e02cbd155c
Alex Markuze <alexm@aptoslabs.com>
2022-11-27 19:45:08 +0200

    Adding kernel headers and scripts for docker

---
+++ headers/5.4.196-108.356.amzn2.x86_64/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#5 SMP Thu Nov 17 10:03:34 UTC 2022"
+#define LINUX_COMPILE_BY "admin"
+#define LINUX_COMPILE_HOST "ip-172-31-8-153"
+#define LINUX_COMPILER "gcc version 10.2.1 20210110 (Debian 10.2.1-6)"
---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DEBUGFS="/sys/kernel/debug"
+KERNEL_VERSION="${KERNEL_VERSION:-$(uname -r)}"
+HEADERS="${HEADERS:-/bcc/headers/$KERNEL_VERSION}"
+
+function die {
+	echo "$@"
+	exit -1
+}
+
+[ ! -d ${HEADERS} ] && die "No such file $HEADERS"
+
+if [[ ! -e /lib/modules/.installed ]]; then
+	echo "Linking /lib/modules/${KERNEL_VERSION}"
+	mkdir -p "/lib/modules/${KERNEL_VERSION}"
+	ln -sf "${HEADERS}" "/lib/modules/${KERNEL_VERSION}/source"
+	ln -sf "${HEADERS}" "/lib/modules/${KERNEL_VERSION}/build"
+	touch /lib/modules/.installed
+fi
+
+if [ ! -d ${DEBUGFS}/tracing ]; then
+	echo "mounting $DEBUGFS"
+	mount -t debugfs none $DEBUGFS
+fi
+echo "BCC setup done"
